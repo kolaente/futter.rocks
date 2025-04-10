003f435a1c31a294eef89f38753c299c4022934d
kolaente <k@knt.li>
2025-04-10 14:46:02 +0200

    feat: show participant groups on event

---
+++ app/Livewire/Events/ListGroups.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Livewire\Events;
+
+use App\Models\Event;
+use Filament\Forms\Concerns\InteractsWithForms;
+use Filament\Forms\Contracts\HasForms;
+use Filament\Forms;
+use Filament\Tables;
+use Filament\Tables\Actions\AttachAction;
+use Filament\Tables\Concerns\InteractsWithTable;
+use Filament\Tables\Contracts\HasTable;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Support\Facades\Auth;
+use Livewire\Component;
+use Illuminate\Contracts\View\View;
+
+class ListGroups extends Component implements HasForms, HasTable
+{
+    use InteractsWithForms;
+    use InteractsWithTable;
+
+    public Event $event;
+
+    public function mount(Event $event)
+    {
+        $this->event = $event;
+    }
+
+    public function table(Table $table): Table
+    {
+        return $table
+            ->relationship(fn (): BelongsToMany => $this->event->participantGroups())
+            ->inverseRelationship('events')
+            ->columns([
+                Tables\Columns\TextColumn::make('title')
+                    ->label(__('Title')),
+                Tables\Columns\TextColumn::make('quantity')
+                    ->label(__('Quantity'))
+                    ->summarize(Tables\Columns\Summarizers\Sum::make()->label(__('Total'))),
+            ])
+            ->headerActions([
+                Tables\Actions\AttachAction::make()
+                    ->preloadRecordSelect()
+                    ->recordSelectOptionsQuery(fn (Builder $query) => $query->where('team_id', Auth::user()->currentTeam->id))
+                    ->form(fn(AttachAction $action): array => [
+                        $action->getRecordSelect(),
+                        Forms\Components\TextInput::make('quantity')
+                            ->numeric()
+                            ->minValue(1)
+                            ->default(1)
+                            ->required(),
+                    ]),
+            ])
+            ->actions([
+                Tables\Actions\DetachAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DetachBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public function render(): View
+    {
+        return view('livewire.events.list-groups');
+    }
+}
