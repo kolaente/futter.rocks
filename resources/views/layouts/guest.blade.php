cd95a3ab03265e8f6afb7c8b4cd3653c6ae25d0d
kolaente <k@knt.li>
2025-04-17 19:09:54 +0200

    feat: add favicon logo

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -7,6 +7,10 @@
 
         <title>{{ config('app.name', 'Laravel') }}</title>
 
+        <link rel="icon" type="image/png" href="{{ asset('/favicon-96x96.png') }}" sizes="96x96" />
+        <link rel="icon" type="image/svg+xml" href=" {{ asset('/favicon.svg') }}" />
+        <link rel="shortcut icon" href=" {{ asset('/favicon.ico') }}" />
+
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
 
