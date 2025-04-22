20bba30e2da1608f066226f9d06ebd616d3ddd97
kolaente <k@knt.li>
2025-04-22 23:02:17 +0200

    feat: add privacy policy text

--- resources/views/policy.blade.php
+++ resources/views/policy.blade.php
@@ -1,11 +1,11 @@
-<x-guest-layout>
-    <div class="pt-4 bg-gray-100 dark:bg-gray-900">
+<x-guest-layout :title="__('Privacy Policy')">
+    <div class="pt-4 bg-gray-100">
         <div class="min-h-screen flex flex-col items-center pt-6 sm:pt-0">
             <div>
                 <x-application-logo />
             </div>
 
-            <div class="w-full sm:max-w-2xl mt-6 p-6 bg-white dark:bg-gray-800 shadow-md overflow-hidden sm:rounded-lg prose dark:prose-invert">
+            <div class="w-full sm:max-w-6xl mt-6 p-6 bg-white shadow-md overflow-hidden sm:rounded-lg prose prose-h1:font-display prose-h2:font-display prose-h3:font-display prose-h4:font-display prose-h5:font-display prose-h6:font-display">
                 {!! $policy !!}
             </div>
         </div>
