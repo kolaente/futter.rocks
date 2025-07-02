c49f18f9348183d7a6f72829a28b41f8638f5da7
kolaente <k@knt.li>
2025-07-02 11:33:36 +0200

    fix(i18n): better wording

---
+++ lang/vendor/filament-forms/de/components.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+
+    'select' => [
+
+        'placeholder' => 'Eine Option auswählen',
+
+        'search_prompt' => 'Beginne mit der Eingabe, um zu suchen...',
+
+    ],
+
+];
